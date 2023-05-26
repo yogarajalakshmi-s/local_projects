7dc677877c8af40bae810349f7a097e648094582
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-05-26 10:28:25 +0530

    exception handling for invalid input

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pandas
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 nato_phonetics_dict = {row.letter: row.code for (index, row) in data.iterrows()}  # Pandas Dataframe Iteration
-word = input("Enter a word: ").upper()
-new_list = [nato_phonetics_dict[letter] for letter in word]
-print(new_list)
+
+valid_word = False
+while not valid_word:
+    word = input("Enter a word: ").upper()
+    try:
+        new_list = [nato_phonetics_dict[letter] for letter in word]
+    except KeyError:
+        print("Enter only alphabets!")
+    else:
+        new_list = [nato_phonetics_dict[letter] for letter in word]
+        print(new_list)
+        valid_word = True
