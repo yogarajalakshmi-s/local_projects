1fbeac483c3b3b6323f4d7ae594bd44b8ac26be7
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-05-27 11:32:10 +0530

    pandas - to_csv('', index=False), to_dict(orient='records')

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ with open("../../Desktop/file_1.txt") as file:
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-# DAY - 25: READING CSV DATA & PANDAS LIBRARY
+# DAY - 25: READING CSV DATA
 # https://pandas.pydata.org/docs/getting_started/index.html#getting-started
 
 with open("weather_data.csv") as file:
@@ -137,57 +137,10 @@ with open("weather_data.csv") as file:
             temperature.append(int(row[1]))
     print(temperature)
 
-# IMPORTANT
-import pandas
-data = pandas.read_csv("weather_data.csv")
-print(data)
-print(f"\n{data['temp']}")
-
-# https://pandas.pydata.org/docs/reference/frame.html
-# DataFrame conversion to a dictionary
-data_dict = data.to_dict()
-print(data_dict)
-
-# https://pandas.pydata.org/docs/reference/series.html
-# Series conversion to a list
-temperature_list = data['temp'].to_list()
-print(temperature_list)
-
-# Finding average of the Series
-avg_temp = round(sum(temperature_list) / len(temperature_list), 2)
-print(avg_temp)
-# (or)
-print(data['temp'].mean())
-
-# Finding max values of the Series
-print(data['temp'].max())
-
-# Getting data in columns
-print(data['temp'])
-# (or) as an attribute
-print(data.temp)
-
-# Getting data from rows
-print(data[data.day == 'Monday'])
-print(data[data.temp == data.temp.max()])  # -> Finding the row with the maximum temperature
-
-# Getting data from a row
-monday = data[data.day == 'Monday']
-print(monday.temp)
-print((monday.temp * 9/5) + 32)  # To Fahrenheit
-
-# Creating a DataFrame from scratch
-data_dict_1 = {
-    "students": ['A', 'B', 'C'],
-    "scores": [98, 99, 100]
-}
-scores = pandas.DataFrame(data_dict_1)
-print(scores)
-scores.to_csv('student_data.csv')
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# DAY -26 : LIST AND DICTIONARY COMPREHENSION, PANDA DATAFRAME ITERATION
+# DAY -26 : LIST AND DICTIONARY COMPREHENSION
 
 numbers = [1, 2, 3]
 new_numbers = [n + 1 for n in numbers]  # -> list comprehension
@@ -216,29 +169,3 @@ print(scores)
 # Conditional dictionary comprehension
 passed_students = {student: score for (student, score) in scores.items() if score > 60}
 print(passed_students)
-
-
-import pandas
-
-# Iterating over pandas DataFrame - dictionary comprehension
-student_dict = {
-    'student': ['Angela', 'Jack', 'Lily'],
-    'score': [78, 88, 98]
-}
-
-# Looping through dictionaries
-for (key, value) in student_dict.items():
-    print(value)
-
-# Loop through dataframe
-student_data_frame = pandas.DataFrame(student_dict)
-print(student_data_frame)
-
-for (key, value) in student_data_frame.items():
-    print(value)
-
-# Pandas has an in-built loop which iterates through the rows - iterrows
-for (index, row) in student_data_frame.iterrows():
-    # print(row)
-    print(row.student)
-
