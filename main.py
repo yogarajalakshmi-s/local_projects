5b98dd30a50f654ce08cdb0af8a7e023d37863a7
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-05-28 13:29:34 +0530

    bug fix

--- main.py
+++ main.py
@@ -5,31 +5,31 @@ import pandas
 
 PLACEHOLDER = "[NAME]"
 EMAIL = "python.test.googol@gmail.com"
-PASSWORD = "test1234"
+PASSWORD = "nrvtagipjcldjmbm"
 
 # Solution - 1:
-data = pandas.read_csv('birthdays.csv')
-names = {}
-emails = {}
-for (key, value) in data.iterrows():
-    names[f"{value.month}-{value.day}"] = value.name
-    emails[f"{value.month}-{value.day}"] = value.email
-
-now = dt.datetime.now()
-today = f"{now.month}-{now.day}"
-if today in names.keys():
-    with open(f"letter_templates/letter_{random.randint(1, 3)}") as letter:
-        content = letter.read()
-        content.replace(PLACEHOLDER, str(names[today]))
-
-        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-            connection.starttls()
-            connection.login(user=EMAIL, password=PASSWORD)
-            connection.sendmail(
-                from_addr=EMAIL,
-                to_addrs=emails[today],
-                msg=f"Subject: HAPPY BIRTHDAY!!\n\n{content}"
-            )
+# data = pandas.read_csv('birthdays.csv')
+# names = {}
+# emails = {}
+# for (key, value) in data.iterrows():
+#     names[f"{value.month}-{value.day}"] = value.name
+#     emails[f"{value.month}-{value.day}"] = value.email
+#
+# now = dt.datetime.now()
+# today = f"{now.month}-{now.day}"
+# if today in names.keys():
+#     with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as letter:
+#         content = letter.read()
+#         content.replace(PLACEHOLDER, str(names[today]))
+#
+#         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#             connection.starttls()
+#             connection.login(user=EMAIL, password=PASSWORD)
+#             connection.sendmail(
+#                 from_addr=EMAIL,
+#                 to_addrs=emails[today],
+#                 msg=f"Subject: HAPPY BIRTHDAY!!\n\n{content}"
+#             )
 
 
 # Solution - 2:
