e04154e29f77272e513286da2ed5526d66b93b18
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-06-01 11:41:30 +0530

    stock news alert - stock, news and twilio api

--- main.py
+++ main.py
@@ -1,195 +1,56 @@
-# Day - 16 - OOP
+import requests
+from datetime import datetime, timedelta
+from newsapi import NewsApiClient
+from twilio.rest import Client
+import os
 
-# Accessing and modifying object attributes and accessing object methods
-# https://docs.python.org/3/library/turtle.html
+# https://www.alphavantage.co/documentation/
 
-from turtle import Turtle, Screen
-turtle_obj = Turtle()
-turtle_obj.shape('turtle')
-turtle_obj.color('red', 'green')
-turtle_obj.forward(100)
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+stock_parameters = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK,
+    "outputsize": "compact",
+    "apikey": os.environ.get('STOCK_API_KEY')
+}
 
+stock_response = requests.get(url="https://www.alphavantage.co/query", params=stock_parameters)
+stock_response.raise_for_status()
+stock_data = stock_response.json()['Time Series (Daily)']
 
-# Creating Table
-# https://code.google.com/archive/p/prettytable/wikis/Tutorial.wiki
+today = datetime.now().date()
+yesterday = str(today - timedelta(days=1))
+day_before_yesterday = str(today - timedelta(days=2))
+yesterday_close = float(stock_data[yesterday]['4. close'])
+day_before_yesterday_close = float(stock_data[day_before_yesterday]['4. close'])
 
-from prettytable import PrettyTable
+close_diff = round(((yesterday_close - day_before_yesterday_close)/day_before_yesterday_close) * 100)
 
-table = PrettyTable()
-table.add_column('Pokemon name', ['Pikachu', 'Squirtle', 'Charmandar'])
-table.add_column('Type', ['Electric', 'Water', 'Fire'])
-table.align = 'l'
-print(table)
+movement = "🔺" if close_diff > 0 else "🔻"
+msg_content = f"{STOCK}: {movement}{abs(close_diff)}%\n\n"
 
+# https://newsapi.org/docs/client-libraries/python
+newsapi = NewsApiClient(api_key=os.environ.get('NEWS_API_KEY'))
+all_articles = newsapi.get_everything(q=COMPANY_NAME,
+                                      from_param=yesterday,
+                                      to=day_before_yesterday,
+                                      language='en',
+                                      sort_by='popularity',
+                                      page=1)
 
-# ---------------------------------------------------------------------------------------------------------------------
+articles = all_articles['articles']
+for article in articles[:3]:
+    msg_content += f"Headline: {article['title']}\nBriefing: {article['description']}\n\n"
 
-# DAY - 19: Event Listeners
-from turtle import Turtle, Screen
+# https://www.twilio.com/docs/sms/send-messages
+client = Client(os.environ.get('ACCOUNT_SID'), os.environ.get('AUTH_TOKEN'))
+message = client.messages \
+    .create(
+        body=msg_content,
+        from_=os.environ.get('FROM_NUMBER'),
+        to=os.environ.get('TO_NUMBER')
+    )
 
-snoopy = Turtle()
-screen = Screen()
-
-def move_forward():
-    snoopy.forward(10)
-
-
-screen.listen()
-screen.onkey(key='space', fun=move_forward)
-screen.exitonclick()
-
-
-# Functions as Inputs - IMPORTANT
-def add(n1, n2):
-    return n1 + n2
-
-def sub(n1, n2):
-    return n1 - n2
-
-def calc(n1, n2, func): # -> Higher Order Function
-    return func(n1, n2)
-
-print(calc(2, 3, add))
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-# DAY - 21 : INHERITANCE
-class Animal:
-    def __init__(self):
-        self.eyes = 2
-
-    def breathe(self):
-        print("Inhale. Exhale.")
-
-class Fish(Animal):
-    def __init__(self):
-        super().__init__()
-
-    def breathe(self):
-        super().breathe()
-        print("Breathe underwater.")
-
-
-nemo = Fish()
-nemo.breathe()
-
-# ---------------------------------------------------------------------------------------------------------------------
-# DAY -24 - Open, Read, Write and Close Files and Absolute and Relative paths
-
-# 1. Open, read and close
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
-
-# 2.Open and read without need of closing the file. This syntax automatically closes the file
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
-
-# 3. Write in a file. (Replaces all contents)
-with open("my_file.txt", mode='w') as file:
-    file.write("New Text.")
-
-# 4. Write in a file without clearing the previous content
-with open("my_file.txt", mode='a') as file:
-    file.write("\nHello World! My name is Yoga!")
-
-# 5. If we mention a non-existing file in write mode, it creates that file
-with open("new_file.txt", mode='w') as file:
-    file.write("My new file!")
-
-
-# 6. Read file from a different directory - say Desktop - Absolute path
-# Replace with the correct username
-with open("/Users/{username}/Desktop/file_1.txt") as file:
-    contents = file.read()
-    print(contents)
-
-# 7. Read file from a different directory - Relative path
-# main.py -> path -> /Users/{username}/PycharmProjects/LocalProjects
-# In the code, we are going backwards by two folders (../../) i.e., we are going to PycharmProjects from LocalProjects
-# And now we are in the Folder {username}. From here, we can trace the path to Desktop and to the required file.
-with open("../../Desktop/file_1.txt") as file:
-    contents = file.read()
-    print(contents)
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-# DAY - 25: READING CSV DATA
-# https://pandas.pydata.org/docs/getting_started/index.html#getting-started
-
-with open("weather_data.csv") as file:
-    data = file.readlines()
-    print(data)
-
-import csv
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
-    print(data)
-    temperature = []
-    for row in data:
-        if row[1] != 'temp':
-            temperature.append(int(row[1]))
-    print(temperature)
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-# DAY -26 : LIST AND DICTIONARY COMPREHENSION
-
-numbers = [1, 2, 3]
-new_numbers = [n + 1 for n in numbers]  # -> list comprehension
-print(new_numbers)
-
-name = "Angela"
-new_list = [letter for letter in name]
-print(new_list)
-
-doubled_list = [n*2 for n in range(1, 5)]
-print(doubled_list)
-
-names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-shorter_names = [name for name in names if len(name) < 5]  # -> Conditional list comprehension
-print(shorter_names)
-
-upcase_names = [name.upper() for name in names if len(name) > 5]
-print(upcase_names)
-
-
-import random
-names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-scores = {student: random.randint(1, 100) for student in names}  # -> Dictionary Comprehension
-print(scores)
-
-# Conditional dictionary comprehension
-passed_students = {student: score for (student, score) in scores.items() if score > 60}
-print(passed_students)
-
-
-# --------------------------------------------------------------------------------------------------------------------
-# DAY - 34: Unescaping HTML entities
-import html
-
-text = "Question&#39;s answer"
-print(html.unescape(text))
-
-
-# IMPORTANT - TYPE HINTS
-age: int
-age = "twelve"  # Hover and see the warning
-
-
-# Using type hints in functions to specify input and output data types
-def name(name: str) -> str:
-    return "Hello " + name + "!"
-
-print(name("Ishu"))
-
-
-
-
+print(message.sid)
