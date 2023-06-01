f9172fd9bd7745b350305931f6010752a6ef79a5
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-06-01 11:50:43 +0530

    datetime - timedelta class

--- datetime_module.py
+++ datetime_module.py
@@ -1,8 +1,7 @@
 # DAY - 33: Datetime module
+from datetime import datetime, timedelta
 
-import datetime as dt
-
-now = dt.datetime.now()
+now = datetime.now()
 print(now)
 year = now.year
 print(year)
@@ -10,6 +9,11 @@ day_of_week = now.weekday()
 print(day_of_week)
 
 # Creating a new datetime object with year, month and date
-date_of_birth = dt.datetime(day=1, month=12, year=2005)  # hour,min,... have default values but d, m, y has to be given.
+date_of_birth = datetime(day=1, month=12, year=2005)  # hour,min,... have default values but d, m, y has to be given.
 print(date_of_birth)
 
+
+today = datetime.now().date()
+yesterday = str(today - timedelta(days=1))  # Getting yesterday as a datetime object
+day_before_yesterday = str(today - timedelta(days=2))
+print(today, yesterday)
